c60891dcde0726acd4eab96f70822a411280f64d
Klim Korovkin <klimkorovkin@yandex.ru>
2023-02-19 15:00:31 +0000

    React + Rust demo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import init, { add } from "wasm-lib";
 
 function App() {
+	const [number, setNumber] = useState(0);
+	useEffect(() => {
+		init().then(() => {
+			setNumber(add(2, 2));
+		});
+	}, []);
+
 	return (
 		<div className="App">
 			<h1>Hello fractal world!</h1>
+			<p>The number is {number}</p>
 		</div>
 	);
 }
